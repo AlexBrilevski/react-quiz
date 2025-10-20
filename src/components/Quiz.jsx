08a7332688eed591ea4d73a4b28638e57f31f936
Alex Brilevski <brilevski.alex@gmail.com>
2025-10-20 20:04:08 +0300

    Outputting active question and processing answers were added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+import QUESTIONS_DATA from '../questions.js';
+
 export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers(prevAnswers => [...prevAnswers, selectedAnswer]);
+  }
+
   return (
-    <p>Currently active question</p>
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS_DATA[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS_DATA[activeQuestionIndex].answers.map(answer => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
